3a3da455e192e7a28979434c9db551276cfbee89
_dream <will.ferens@gmail.com>
2018-10-05 11:17:12 -0600

    functional to do list app

---
+++ ToDo/Controllers/CategoryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CategoryViewController.swift
+//  ToDo
+//
+//  Created by Will E Ferens on 10/5/18.
+//  Copyright © 2018 Will Ferens. All rights reserved.
+//
+
+import Foundation
---
+++ ToDo/Controllers/ToDoListViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Will E Ferens on 10/4/18.
+//  Copyright © 2018 Will Ferens. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ToDoListViewController: UITableViewController {
+
+    var itemArray = [Item]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        loadItems()
+    }
+
+    //MARK: - TableView Datasource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New ToDo", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func saveItems() {
+        
+        do {
+           try context.save()
+        } catch {
+            print("error saving contex \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
+        do {
+           itemArray = try context.fetch(request)
+        } catch {
+            print("error fetching data \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    
+}
+//MARK: - Search bar methods
+extension ToDoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
+}
+
